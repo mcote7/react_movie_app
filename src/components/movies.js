c7d18aa663efa1ec18090036453bb6a13d04e127
Michael Cote <michaelcote7@outlook.com>
2020-06-11 16:18:19 -0700

    delete all and sold out back button added .

--- src/components/movies.js
+++ src/components/movies.js
@@ -8,13 +8,34 @@ class Movies extends Component {
 
   handleDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
-    this.setState({ movies: movies });
+    this.setState({ movies });
   };
 
+  deleteAll = () => {
+    this.setState({movies: ""});
+  }
+
   render() {
+
+    const { length: count} = this.state.movies;
+
+    if(count === 0) {
+      return(
+        <div className="col">
+          <div className="row ml-1">
+            <a className="badge badge-primary d-inline-flex align-items-center mt-2" href="/">
+            we are currently sold out ,&nbsp;&nbsp;&nbsp;&nbsp;go back&nbsp;&rarr;</a>
+          </div>
+        </div>
+      );
+    }
     return (
     <div className="col">
-      <p>Showing {this.state.movies.length} movies in the database</p>
+      <div className="row ml-1">
+        <button className="btn btn-danger btn-sm" onClick={() => this.deleteAll()}>
+        <span role="img" aria-label="img">&#128128;</span>all</button>
+        <p className="ml-4">Showing {this.state.movies.length} movies in the database</p>
+      </div>
       <table className="table">
         <thead>
           <tr>
@@ -26,7 +47,8 @@ class Movies extends Component {
           </tr>
         </thead>
         <tbody>
-          {this.state.movies.sort((a, b) => (a.title > b.title) ? 1 : -1).map((mov) =>
+          {count > 0 && this.state.movies.sort((a, b) => (a.title > b.title) ? 1 : -1)
+          .map((mov) =>
           <tr key={mov._id}>
             <td>{mov.title}</td>
             <td>{mov.genre.name}</td>
