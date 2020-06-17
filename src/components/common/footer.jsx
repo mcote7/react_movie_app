c92ca87987a826f8bbaf094ce2e6a14514b7f0ed
Michael Cote <michaelcote7@outlook.com>
2020-06-16 22:41:48 -0700

    adding routing

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -5,8 +5,8 @@ import logo from './logo192.png'
 const Footer = () => {
   return (
     <nav className="navbar navbar-expand-lg myNav-bottom">
-      <img className="ml-5" src={logo} alt="img-react"/>
-      <p className="ml-5" style={{color: "#61dafb", letterSpacing: "1em"}}>
+      <img className="ml-5 x" src={logo} alt="img-react"/>
+      <p className="ml-5 x" style={{color: "#61dafb", letterSpacing: "1em"}}>
         &copy;&nbsp;2020&nbsp;&nbsp;michael cote
         &nbsp;&nbsp;&nbsp;made with React</p>
       <ScrollUpButton />
--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -5,10 +5,10 @@ const NavBar = () => {
   const smFont = {color: "#61dafb", fontSize: "small"};
   return (
     <nav className="navbar sticky-top navbar-expand-lg myNav-top">
-      <img className="ml-2 navImg" src="favicon.ico" alt="img-react"/>
-      <h5 className="ml-4" style={text}>HOME</h5>
-      <p className="ml-5" style={smFont}>MOVIES</p>
-      <p className="ml-5" style={smFont}>BACK</p>
+      <img className="ml-2 x navImg" src="favicon.ico" alt="img-react"/>
+      <h5 className="ml-2 x" style={text}>HOME</h5>
+      <p className="ml-5 mt-2 x" style={smFont}>MOVIES</p>
+      <p className="ml-5 mt-2 x" style={smFont}>BACK</p>
     </nav>
   );
 };
