46fe8da75c6b6a2c712a1db671f3eba57564ec98
Michael Cote <michaelcote7@outlook.com>
2020-06-16 19:01:48 -0700

    added logos

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ScrollUpButton from 'react-scroll-up-button';
+import logo from './logo192.png'
 
 const Footer = () => {
   return (
     <nav className="navbar navbar-expand-lg myNav-bottom">
-      <img className="ml-5" src="favicon.ico" alt="img-react"/>
+      <img className="ml-5" src={logo} alt="img-react"/>
       <p className="ml-5" style={{color: "#61dafb", letterSpacing: "1em"}}>
         &copy;&nbsp;2020&nbsp;&nbsp;michael cote
         &nbsp;&nbsp;&nbsp;made with React</p>
