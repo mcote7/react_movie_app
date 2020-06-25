e343d9b1b7b017110ed60986337d589df736f336
Michael Cote <michaelcote7@outlook.com>
2020-06-24 21:31:52 -0700

    .

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import auth from '../services/authService';
 import {Link} from "react-router-dom";
 import Table from './common/table';
 import Like from './common/like';
@@ -11,17 +12,26 @@ class MoviesTable extends Component {
     { path: 'numberInStock', label: 'Stock'},
     { path: 'dailyRentalRate', label: 'Rate'},
     { label: 'Like', content: m =>
-    (<Like liked={m.liked} onLike={() => this.props.onLike(m)}/>)},
-    { label: 'Delete', content: m =>
+    (<Like liked={m.liked} onLike={() => this.props.onLike(m)}/>)}
+  ];
+
+  deleteColumn = { label: 'Delete', content: m =>
     (<button className="btn btn-danger btn-sm"
     onClick={() => this.props.onDelete(m, this.props.movLen, this.props.currentPage)}>
     <span role="img" aria-label="img">&#128128;</span></button>)}
-  ];
+
+  constructor() {
+    super();
+    const user = auth.getCurrentUser();
+    if(user && user.isAdmin) this.columns.push(this.deleteColumn);
+  }
+
+
   render() {
     const { count, movies, sortColumn, onSort } = this.props;
     return (
       <Table columns={this.columns} count={count} data={movies} sortColumn={sortColumn} onSort={onSort}/>
     );
-  }
-}
+  };
+};
 export default MoviesTable;
