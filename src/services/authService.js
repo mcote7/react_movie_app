51dab699f6c3b1d77bef2c13fa9308ed7b858486
Michael Cote <michaelcote7@outlook.com>
2020-06-25 22:27:15 -0700

    .

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,7 @@
-import http from './httpService';
-import {apiUrl} from '../config.json';
 import jwtDecode from 'jwt-decode';
+import http from './httpService';
 
-const apiEndpoint = apiUrl+"/auth";
+const apiEndpoint ="/auth";
 const tokenKey = 'token';
 
 http.setJwt(getJwt());
--- src/services/httpService.js
+++ src/services/httpService.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import logger from './logService';
 import {toast} from 'react-toastify';
 
+axios.defaults.baseURL = proccess.env.REACT_APP_API_URL;
+
 axios.interceptors.response.use(null, error => {
   const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
   if(!expectedError){
