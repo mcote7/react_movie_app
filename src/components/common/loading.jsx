eecdd67476426e97905e27d06e86aad8e0b1f37e
Michael Cote <michaelcote7@outlook.com>
2020-07-08 20:56:28 -0700

    .

--- src/components/common/loading.jsx
+++ src/components/common/loading.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loading = () => {
   return (
-    <h1 style={{color: "red"}}><span><i className="fa fa-spinner fa-4x App-logo2" aria-hidden="true"></i></span>&nbsp;&nbsp;Loading . . .</h1>
+    <h1 style={{color: "red"}}><span><i className="fa fa-spinner fa-pulse fa-4x App-logo2" aria-hidden="true"></i></span>&nbsp;&nbsp;Loading . . .</h1>
   );
 }
 
